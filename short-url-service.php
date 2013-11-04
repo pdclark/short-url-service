616de41a66db3f4bb1abbf597efb389ae289c75b
Paul Clark <pdclark@brainstormmedia.com>
2013-11-04 00:52:11 -0900

    Change sedlex menu to Short URLs.

--- short-url-service.php
+++ short-url-service.php
@@ -6,35 +6,17 @@ Description: Provide service for generating URLs with <a href="http://wordpress.
 Version: 1.0
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-
-/**
- * Copyright (c) 2013 Brainstorm Media. All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * **********************************************************************
- */
 
 class Storm_Short_URL_Service {
 
 	static public function init() {
 		add_action( 'wp_ajax_shorten', array( __CLASS__, 'shorten' ) );
 		add_action( 'wp_ajax_nopriv_shorten', array( __CLASS__, 'shorten' ) );
+
+		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ), 20 );
 	}
 
 	static public function check_requirements() {
@@ -92,6 +74,20 @@ class Storm_Short_URL_Service {
 		}else {
 			return $url;
 		}
+	}
+
+	/**
+	 * Replace SedLex menu with Short URLs menu
+	 */
+	static public function admin_menu() {
+		$short_url = shorturl::getInstance();
+
+		remove_menu_page( 'sedlex.php' );
+		remove_submenu_page( 'sedlex.php', 'sedlex.php' );
+		remove_submenu_page( 'sedlex.php', 'shorten-url/shorten-url.php' );
+
+		add_object_page( 'Short URLs', 'Short URLs', 'edit_options', 'shorten-url/shorten-url.php', array( $short_url, 'configuration_page' ) );
+
 	}
 
 }
